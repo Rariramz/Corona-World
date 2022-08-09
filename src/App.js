09876886cd14ad98db22c35ff76fb592c79bc38d
Rariramz <mary4798713@gmail.com>
2022-08-09 18:43:19 +0300

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, { Suspense } from "react";
 import "./nullstyle.css";
 import "./style.css";
-import GlobalCard from "./components/GlobalCard";
-import CountryCard from "./components/CountryCard";
-import ContinentCard from "./components/ContinentCard";
 import Canvas from "./components/Canvas";
+const GlobalCard = React.lazy(() => import("./components/GlobalCard"));
+const CountryCard = React.lazy(() => import("./components/CountryCard"));
+const ContinentCard = React.lazy(() => import("./components/ContinentCard"));
 
 function App() {
   return (
     <>
-      <GlobalCard />
-      <CountryCard />
-      <ContinentCard />
+      <Suspense fallback={
+        <div class="loader">
+          <div class="inner one"></div>
+          <div class="inner two"></div>
+          <div class="inner three"></div>
+        </div>
+      }>
+        <GlobalCard />
+        <CountryCard />
+        <ContinentCard />
+      </Suspense>
       <Canvas />
     </>
   );
